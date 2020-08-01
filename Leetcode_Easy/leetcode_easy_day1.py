6633c73094c397b40ef0f93781405a48fad9d117
Troy_X <56990256+TroyXZW@users.noreply.github.com>
2020-08-01 16:53:57 +0800

    简单题1，7，9，13
    1.两数之和，7.整数反转，9.回文数，13.罗马数字转整数

--- Leetcode_Easy/leetcode_easy_day1.py
+++ Leetcode_Easy/leetcode_easy_day1.py
@@ -24,7 +24,6 @@ def twoSum(nums, target):
 
 print(twoSum(nums, target))
 
-
 # ---------------------------------------------------
 
 def twoSum(self, nums, target):
@@ -138,7 +137,6 @@ def romanToInt(s):
     return toint
 
 # ---------------------------------------------------
-
 
 def romanToInt(s):
     d = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500,
